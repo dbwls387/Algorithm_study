a75b4c4c6d265f9829346953a9576867d15c5b3e
dbwls387 <dbwls387@naver.com>
2021-06-23 17:38:33 +0900

    약수 구하기

---
+++ src/BOJ/No_2501.java
@@ -0,0 +1,29 @@
+//약수 구하기
+
+package BOJ;
+
+import java.util.Scanner;
+
+public class No_2501 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] arr = new int[N]; //약수 들을 저장할 배열
+
+        int q = 0;
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {   //약수
+                arr[q++] = i;
+            }
+        }
+
+        if (arr.length < K) {   //약수의 개수가 K개보다 적을 경우
+            System.out.println(0);  //0을 출력
+        } else {
+            System.out.println(arr[K-1]);   //K번째로 작은 수 출력
+        }
+    }
+}
