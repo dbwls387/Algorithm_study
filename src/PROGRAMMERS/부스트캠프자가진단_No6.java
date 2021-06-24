d0fa53538017ee81235d8ff5f5dba657bb02a81f
dbwls387 <dbwls387@naver.com>
2021-06-24 21:34:04 +0900

    부스트캠프자가진단 6번

--- src/PROGRAMMERS/부스트캠프자가진단_No6.java
+++ src/PROGRAMMERS/부스트캠프자가진단_No6.java
@@ -1,14 +1,15 @@
+//hashmap 사용해서 다시 풀었음
+
 package PROGRAMMERS;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class 부스트캠프자가진단_No6 {
     public static void main(String[] args) {
         //int[] arr = {1, 2, 3, 3, 3, 3, 4, 4};  //테스트 케이스 1
-        int[] arr = {3, 2, 4, 4, 2, 5, 2, 5, 5};  //테스트 케이스 2
+        //int[] arr = {3, 2, 4, 4, 2, 5, 2, 5, 5};  //테스트 케이스 2
         //int[] arr = {3, 5, 7, 9, 1};    //테스트 케이스 3
+        int[] arr = {3, 2, 5, 5, 4, 4, 2, 5, 2, 5, 5};
 
         List<Integer> answer;
         answer = Solution(arr);
@@ -17,24 +18,25 @@ public class 부스트캠프자가진단_No6 {
 
     private static List<Integer> Solution (int [] arr) {
         List<Integer> answer = new ArrayList<>();
+        Map<Integer, Integer> map = new LinkedHashMap<>();
 
-        Arrays.sort(arr);
-
-        int cnt = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i - 1] == arr[i]) {
-                cnt++;
-            }
-            if (cnt > 0) {
-                if ((arr[i] != arr[i - 1]) || (i == arr.length - 1)) {
-                    answer.add(cnt + 1);
-                    cnt = 0;
-                }
+        for (int a : arr) {
+            if(map.get(a) != null){
+                int cnt = map.get(a);
+                map.put(a, ++cnt);
+            } else{
+                map.put(a, 1);
             }
         }
-        if (answer.size() == 0) {
+        for(int key : map.keySet()){
+            if(map.get(key) > 1){
+                answer.add(map.get(key));
+            }
+        }
+        if(answer.size() == 0){
             answer.add(-1);
         }
+
         return answer;
     }
 }
