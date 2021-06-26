b5a66a2038e9c85016428a66db5cccc43af51645
dbwls387 <dbwls387@naver.com>
2021-06-27 01:35:49 +0900

    사칙연산

---
+++ src/BOJ/Mathematics/No_10869.java
@@ -0,0 +1,18 @@
+//사칙연산
+
+package BOJ.Mathematics;
+
+import java.util.Scanner;
+public class No_10869 {
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
