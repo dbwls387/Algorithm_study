3d4ba2c820fb2bdaa3e6fd8d3e1d5f505cb3d2a7
dbwls387 <dbwls387@naver.com>
2021-06-27 01:28:03 +0900

    A/B

---
+++ src/BOJ/Mathematics/No_1008.java
@@ -0,0 +1,20 @@
+//A / B
+
+package BOJ.Mathematics;
+
+import java.util.Scanner;
+
+public class No_1008 {
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        double A;
+        double B;
+        double result;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        result = A/B;
+        System.out.println(result);
+    }
+}
