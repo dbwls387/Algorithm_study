26b1c53d57ca662289cf7e0e07271ad0b1ef592c
dbwls387 <dbwls387@naver.com>
2021-12-29 09:27:59 +0900

    냉동식품

---
+++ src/BOJ/Implementation/No_14625.py
@@ -0,0 +1,19 @@
+# 14625 냉동식품
+
+h1, m1 = map(int, input().split())
+h2, m2 = map(int, input().split())
+n = int(input())
+cnt = 0
+h_current = h1
+m_current = m1
+while True:
+    if h_current % 10 == n or h_current // 10 == n or m_current % 10 == n or m_current // 10 == n:
+        cnt += 1
+    if h2 == h_current and m2 == m_current:
+        break
+    m_current += 1
+    if m_current == 60:
+        h_current += 1
+        m_current = 0
+
+print(cnt)
