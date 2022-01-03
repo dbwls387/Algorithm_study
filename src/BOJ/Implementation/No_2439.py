9ed2945174d1d77f69096e43ed727cc124981b82
dbwls387 <dbwls387@naver.com>
2022-01-04 00:37:38 +0900

    별 찍기-2

---
+++ src/BOJ/Implementation/No_2439.py
@@ -0,0 +1,10 @@
+# 2439 별 찍기-2
+
+n = int(input())
+
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end='')
+    for k in range(i + 1):
+        print("*", end='')
+    print("")
