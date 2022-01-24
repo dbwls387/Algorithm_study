05b72a86d081cb89bb532a8278a81a1f1a66aa96
dbwls387 <dbwls387@naver.com>
2022-01-24 15:53:33 +0900

    신입 사원

--- src/BOJ/Greedy/No_1946.py
+++ src/BOJ/Greedy/No_1946.py
@@ -15,3 +15,25 @@ for _ in range(t):
             cnt += 1
 
     print(cnt)
+
+### 처음 짠 코드 -> 시간초과 떠서 실패
+# import sys
+# t = int(input())
+# for a in range(t):
+#     n = int(input())
+#     arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+#     arr.sort(reverse=True, key=lambda x: (x[0]))
+#
+#     i = 0
+#     cnt = 0
+#     while i != len(arr):
+#         i = 0
+#         for j in range(1, len(arr)):
+#             if arr[i][1] > arr[j][1]:
+#                 arr.pop(i)
+#                 break
+#         else:
+#             cnt += 1
+#             arr.pop(i)
+#
+#     print(cnt)
