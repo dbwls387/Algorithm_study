ca7e4323bbe4b73a64e00d39063c4cc600313822
dbwls387@naver.com <dbwls387@naver.com>
2022-02-14 23:46:09 +0900

    예산

---
+++ src/BOJ/Binary_Search/No_2512.py
@@ -0,0 +1,27 @@
+# 2512 예산
+
+n = int(input())
+arr = list(map(int, input().split()))
+m = int(input())
+
+
+def binary(arr):
+    start = 0
+    end = max(arr)
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        for i in range(n):
+            if arr[i] >= mid:
+                cnt += mid
+            else:
+                cnt += arr[i]
+
+        if cnt <= m:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return end
+
+
+print(binary(arr))
