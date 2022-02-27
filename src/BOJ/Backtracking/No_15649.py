1364e1dd3284d1ba4ebb29b5f1ce85c460378882
dbwls387@naver.com <dbwls387@naver.com>
2022-02-28 00:12:44 +0900

    N과 M (1)

---
+++ src/BOJ/Backtracking/No_15649.py
@@ -0,0 +1,19 @@
+# N과 M (1)
+
+n, m = map(int, input().split())
+solve = []
+
+def dfs(depth):
+    if depth == m:
+        for i in solve:
+            print(i, end=' ')
+        print()
+        return
+
+    for i in range(1, n + 1):
+        if i not in solve:
+            solve.append(i)
+            dfs(depth + 1)
+            solve.pop()
+
+dfs(0)
