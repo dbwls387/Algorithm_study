3f5e1035dc6165f9088e7626832e0faea000d326
dbwls387@naver.com <dbwls387@naver.com>
2022-03-20 22:45:38 +0900

    바이러스

---
+++ src/BOJ/Dfs & Bfs/No_2606.py
@@ -0,0 +1,30 @@
+# 2606 바이러스
+
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(v):
+    global cnt
+    visited[v] = 1
+    for i in graph[v]:
+        if visited[i] == 0:
+            dfs(i)
+            cnt += 1
+
+
+n = int(input())
+m = int(input())
+graph = [[] * n for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+cnt = 0
+visited = [0] * (n + 1)
+
+dfs(1)
+print(cnt)
