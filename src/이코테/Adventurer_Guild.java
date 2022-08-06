84b0e37a414e5560309a6b77611c36d4ff5f233e
dbwls387@naver.com <dbwls387@naver.com>
2022-08-06 14:32:04 +0900

    모험가의 길드 (수정)

--- src/이코테/Adventurer_Guild.java
+++ src/이코테/Adventurer_Guild.java
@@ -23,16 +23,22 @@ public class Adventurer_Guild {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int person = 0;
+		// 정렬해줘야 함! (놓쳤던 부분.. 코드 리뷰를 통해 알게 됨)
+		Arrays.sort(arr);	
+		
+		int person = 0;	
 		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			person++;
+		// 2 3 1 2 2 
+		// 1 2 2 2 3
+		for (int i = 0; i < N; i++) {	
+			person++;	
 			if (person >= arr[i]) {
+				// 한 그룹의 모험가의 수가 현재 모험가의 공포도를 넘어선다면
+				// 팀 수 ++ / 세던 모험가 수 초기화
 				cnt++;
 				person = 0;
 			}
 		}
 		System.out.println(cnt);
 	}
-
 }
