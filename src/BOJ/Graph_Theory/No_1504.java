6a0d33091c0022a15eea99deeb3a4f0e04b58485
dbwls387 <dbwls387@gmail.com>
2022-08-30 22:31:45 +0900

    특정한 최단 경로 푸는 중..

---
+++ src/BOJ/Graph_Theory/No_1504.java
@@ -0,0 +1,26 @@
+// BOJ 1504번 특정한 최단 경로 
+
+package BOJ.Graph_Theory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class No_1504 {
+
+	static int N, E;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < E; i++) {
+			
+		}
+	}
+
+}
