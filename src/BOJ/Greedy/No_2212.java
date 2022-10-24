60f0b248226fe757c0ee2c8b3d52f44822f404f0
dbwls387@naver.com <dbwls387@naver.com>
2022-10-24 22:33:27 +0900

    센서

--- src/BOJ/Greedy/No_2212.java
+++ src/BOJ/Greedy/No_2212.java
@@ -1,8 +1,11 @@
+// BOJ 2212번 센서 
+
 package BOJ.Greedy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class No_2212 {
@@ -25,18 +28,15 @@ public class No_2212 {
 		if (K >= N) {
 			System.out.println(0);
 		} else {
-
 			Integer[] dif = new Integer[N - 1]; // 센서들간의 차이 구하기
 			for (int i = 0; i < N - 1; i++) {
 				dif[i] = arr[i + 1] - arr[i];
 			}
 
-//		Arrays.sort(dif, Collections.reverseOrder()); // 차이 내림차순 정렬
-			Arrays.sort(dif);
-			System.out.println(Arrays.toString(dif));
+			Arrays.sort(dif, Collections.reverseOrder()); // 차이 내림차순 정렬
 
 			int ans = 0;
-			for (int i = 0; i < K; i++) {
+			for (int i = K - 1; i < N - 1; i++) { // 간격이 젤 큰 것 부터 K-1개는 빼고 더함
 				ans += dif[i];
 			}
 
