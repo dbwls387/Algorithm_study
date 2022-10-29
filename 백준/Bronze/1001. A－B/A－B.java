af715618d7ae0de9f261df764d8854e3af99e33a
dbwls387 <52234053+dbwls387@users.noreply.github.com>
2022-10-29 23:48:36 +0900

    [Bronze V] Title: A-B, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        int A;
+        int B;
+        int result;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        result = A-B;
+
+        System.out.println(result);
+    }
+}
