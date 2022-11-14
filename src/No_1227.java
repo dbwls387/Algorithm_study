55cdab18e71676073fcd578086171eb61a9f3416
dbwls387 <dbwls387@naver.com>
2022-11-15 00:34:07 +0900

    미로 2 푸는 중..

---
+++ src/No_1227.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// SWEA 1227번  미로 2
+
+public class No_1227 {
+
+	static int sy, sx, ey, ex;
+	static int[][] map;
+
+	static boolean[][] visit;
+
+	static int[] dy = { -1, 1, 0, 0 };
+	static int[] dx = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int t = 1; t <= 10; t++) {
+			map = new int[100][100];
+			for (int i = 0; i < 100; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < 100; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					if (map[i][j] == 2) {
+						sy = i;
+						sx = j;
+					} else if (map[i][j] == 3) {
+						ey = i;
+						ex = j;
+					}
+				}
+			}
+
+			bfs();
+		}
+
+	}
+
+	static void bfs() {
+		Queue<Node> que = new ArrayDeque<>();
+		que.offer(new Node(sy, sx));
+
+		while (!que.isEmpty()) {
+			Node node = que.poll();
+
+			for (int d = 0; d < 4; d++) {
+				int ny = node.y + dy[d];
+				int nx = node.x + dx[d];
+				
+				
+			}
+			
+		}
+
+	}
+
+	static class Node {
+		int y, x;
+
+		public Node(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+	}
+
+}
