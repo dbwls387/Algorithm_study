2ac256bacb3625c7414cef9a89a6bd5465f8cbe6
dbwls387 <52234053+dbwls387@users.noreply.github.com>
2022-11-27 18:52:11 +0900

    [Gold IV] Title: 최단경로, Time: 996 ms, Memory: 109652 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -5,84 +5,84 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+// BOJ - No_1753 최단 경로
+
 public class Main {
 
 	static int V, E, K;
 	static List<List<Edge>> vertex = new ArrayList<>();
+	static int[] cost;
 	static boolean[] visit;
-	static int[] cost; // 핵심
-	static PriorityQueue<Edge> pqueue = new PriorityQueue<>((e1, e2) -> e1.c - e2.c);
-	static final int INF = Integer.MAX_VALUE;
+
+	static int INF = Integer.MAX_VALUE;
+
+	static PriorityQueue<Edge> pque = new PriorityQueue<>((e1, e2) -> e1.c - e2.c);
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(br.readLine());
 
 		visit = new boolean[V + 1];
 		cost = new int[V + 1];
-
 		for (int i = 0; i <= V; i++) {
 			vertex.add(new ArrayList<Edge>());
-			cost[i] = INF; // 충분히 큰 값으로
+			cost[i] = INF;
 		}
 
-		// edge
-		for (int i = 0; i < E; i++) {
+		for (int i = 1; i <= E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int v1 = Integer.parseInt(st.nextToken());
 			int v2 = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 
-			// 방향
 			vertex.get(v1).add(new Edge(v2, w));
 		}
 
 		dijkstra();
 
 		for (int i = 1; i <= V; i++) {
-			System.out.println(cost[i] == INF ? "INF" : cost[i]);
+			if (cost[i] == INF)
+				System.out.println("INF");
+			else
+				System.out.println(cost[i]);
 		}
 	}
 
 	static void dijkstra() {
-		// 시작 정점 K
+		pque.offer(new Edge(K, 0));
 		cost[K] = 0;
-		pqueue.add(new Edge(K, 0));
 
-		while (!pqueue.isEmpty()) {
-			// 꺼내면 비용이 가장 작은 것
-			Edge e = pqueue.poll();
+		while (!pque.isEmpty()) {
+			Edge e = pque.poll();
 
 			if (e.c > cost[e.v])
-				continue; // 가지치기
+				continue;
 
-			// visit check
 			if (visit[e.v])
 				continue;
 
-			// e.v 정점으로부터 갈 수 있는 다른 정점을 고려
-			// 고려하는 목적은 cost[] 갱신하기 위해
 			visit[e.v] = true;
 
 			for (Edge ne : vertex.get(e.v)) {
-				if (!visit[ne.v] && cost[e.v] + ne.c < cost[ne.v]) {
+				if (cost[e.v] + ne.c < cost[ne.v]) {
 					cost[ne.v] = cost[e.v] + ne.c;
-					pqueue.offer(new Edge(ne.v, cost[ne.v]));
+					pque.offer(new Edge(ne.v, cost[ne.v]));
 				}
 			}
 		}
 	}
 
 	static class Edge {
-		int v; // 정점
-		int c; // 비용
+		int v, c;
 
 		public Edge(int v, int c) {
 			this.v = v;
 			this.c = c;
 		}
 	}
+
 }
