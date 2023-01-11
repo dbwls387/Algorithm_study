cbce7a5e0889816486aa9adf92ad6d49f367f883
dbwls387 <52234053+dbwls387@users.noreply.github.com>
2023-01-11 22:45:28 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 31.99 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,23 @@
+import java.lang.Math;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" "); 
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0; i<arr.length; i++) {
+            int num = Integer.parseInt(arr[i]); 
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        
+        answer += min; 
+        answer += " "; 
+        answer += max; 
+        
+        return answer;
+    }
+}
