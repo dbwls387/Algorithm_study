164e939741c10a4de9bb882e93362d1b2ba8a196
dbwls387 <52234053+dbwls387@users.noreply.github.com>
2023-01-15 16:08:03 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.22 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -4,26 +4,25 @@ class Solution {
         String[] arr = s.split(" ");
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length() == 0) {
+            if (arr[i].length() == 0) { // 공백 연속으로 들어올 경우
                 answer += ' ';
                 continue;
             }
-            char ch = arr[i].charAt(0);
-            if (ch == ' ')
-                answer += ch;
-
-            if (!Character.isDigit(ch)) {
-                answer += Character.toString(ch).toUpperCase();
+            
+            char ch = arr[i].charAt(0); // 단어의 첫 문자
+            if (!Character.isDigit(ch)) {   // 첫 문자가 숫자가 아니면
+                answer += Character.toString(ch).toUpperCase(); // 대문자
             } else {
-                answer += ch;
+                answer += ch;   // 숫자이면 그대로 정답 문자열에 추가
             }
 
-            answer += arr[i].substring(1).toLowerCase();
+            answer += arr[i].substring(1).toLowerCase();    // 나머지 글자는 모두 소문자
 
-            if (i != arr.length - 1)
+            if (i != arr.length - 1)    // 제일 마지막 단어 뒤엔 공백 X
                 answer += ' ';
         }
 
+        // 맨 뒤에 공백이 있을 경우 
         if (s.substring(s.length() - 1).equals(" "))
             answer += ' ';
 
