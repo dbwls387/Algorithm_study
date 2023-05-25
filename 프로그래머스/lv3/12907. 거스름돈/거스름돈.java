7e59985e1716c659c98412cac201ab603d4c66cc
dbwls387 <52234053+dbwls387@users.noreply.github.com>
2023-05-25 11:15:18 +0900

    [level 3] Title: 거스름돈, Time: 11.83 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n + 1];
+        
+        dp[0] = 1;
+        for(int i = 0; i < money.length; i++) {
+            for(int j = money[i]; j <= n; j++) {
+                dp[j] += (dp[j - money[i]]) % 1_000_000_007;
+            }
+        }
+        
+        return dp[n];
+    }
+}
