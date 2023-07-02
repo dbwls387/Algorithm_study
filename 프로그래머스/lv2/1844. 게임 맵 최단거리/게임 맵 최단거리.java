722331fe0dc2ad86c2d8e0d5bc582af5b44021d6
dbwls387 <52234053+dbwls387@users.noreply.github.com>
2023-07-02 15:32:07 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.04 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,61 +2,59 @@ import java.util.*;
 
 class Solution {
     
-    static int N, M, min; 
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1}; 
-    
     static int[][] maps; 
+    static int N, M, answer; 
     
-    public int solution(int[][] maps) {
-        min = Integer.MAX_VALUE;        
+    static int[] dy = {-1, 1, 0, 0}; 
+    static int[] dx = {0, 0, -1, 1};
+    
+    public int solution(int[][] maps) {        
+        this.maps = maps; 
         N = maps.length; 
-        M = maps[0].length;
+        M = maps[0].length; 
         
-        this.maps = maps;
-        
+        answer = 0; 
         bfs();
         
-        if(min == Integer.MAX_VALUE) 
-            min = -1;
-        
-        return min;
+        return answer;
     }
     
     static void bfs() {
         Queue<Node> que = new ArrayDeque<>(); 
-        boolean[][] visit = new boolean[N][M];
+        boolean[][] visit = new boolean[N][M]; 
+        que.offer(new Node(0, 0, 1)); 
+        visit[0][0] = true;
         
-        visit[0][0] = true; 
-        que.add(new Node(0, 0, 1)); 
         while(!que.isEmpty()) {
-            Node node = que.poll();
+            Node node = que.poll(); 
             
-            if(node.y == N-1 && node.x == M-1) {
-                min = Math.min(min, node.count); 
-                
-                return; 
+            if(node.y == N - 1 && node.x == M - 1) {
+                answer = node.dir;
+                return;
             }
             
             for(int d = 0; d < 4; d++) {
                 int ny = node.y + dy[d];
-                int nx = node.x + dx[d];
+                int nx = node.x + dx[d]; 
                 
-                if(ny < 0 || nx < 0 || ny >= N || nx >= M || maps[ny][nx] == 0 || visit[ny][nx]) continue; 
+                if(ny < 0 || nx < 0 || ny >= N || nx >= M || maps[ny][nx] == 0 || visit[ny][nx]) 
+                    continue; 
                 
-                que.add(new Node(ny, nx, node.count + 1)); 
                 visit[ny][nx] = true; 
+                que.offer(new Node(ny, nx, node.dir + 1)); 
             }
         }
+        
+        answer = -1; 
     }
     
     static class Node {
-        int y, x, count;
+        int y, x, dir; 
         
-        public Node(int y, int x, int count) {
+        public Node(int y, int x, int dir) {
             this.y = y;
             this.x = x;
-            this.count = count; 
+            this.dir = dir; 
         }
     }
 }
