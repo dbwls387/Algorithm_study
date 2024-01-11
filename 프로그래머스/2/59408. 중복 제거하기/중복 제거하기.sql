750f96a8d2da54375bc1f6293ebb1b3716505f87
dbwls387 <52234053+dbwls387@users.noreply.github.com>
2024-01-11 18:12:01 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,3 +1,5 @@
 SELECT COUNT(DISTINCT(NAME)) AS count 
-  FROM ANIMAL_INS 
- WHERE NAME IS NOT NULL; 
+  FROM ANIMAL_INS; 
+ 
+ -- COUNT 함수가 NULL을 제거하고 세어주기 때문에 아래 구절은 필요 없음 
+ -- WHERE NAME IS NOT NULL; 
