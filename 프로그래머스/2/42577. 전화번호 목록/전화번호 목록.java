936d0875c3eb7c08353e79f6f72ef7ca9af03d97
dbwls387 <52234053+dbwls387@users.noreply.github.com>
2024-04-24 18:48:15 +0900

    [level 2] Title: 전화번호 목록, Time: 351.74 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -9,7 +9,7 @@ class Solution {
         for(int i = 0; i < phone_book.length - 1; i++) {
             if(phone_book[i + 1].startsWith(phone_book[i])) {
                 answer = false; 
-                break;
+                break; 
             }
         }
         
