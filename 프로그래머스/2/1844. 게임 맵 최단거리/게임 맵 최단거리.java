2c6a2c46ee8f1593fbc83258d5a41abbc8c3c0d0
dbwls387 <52234053+dbwls387@users.noreply.github.com>
2024-05-15 01:29:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.13 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,59 +2,59 @@ import java.util.*;
 
 class Solution {
     
-    static int[][] maps; 
     static int N, M, answer; 
+    static int[][] maps; 
     
-    static int[] dy = {-1, 1, 0, 0};
+    static int[] dy = {-1, 1, 0, 0}; 
     static int[] dx = {0, 0, -1, 1}; 
     
     public int solution(int[][] maps) {
-        this.maps = maps;
-        N = maps.length;
-        M = maps[0].length;
+        answer = Integer.MAX_VALUE;
+        N = maps.length; 
+        M = maps[0].length; 
+        this.maps = maps; 
         
-        answer = Integer.MAX_VALUE; 
         bfs(); 
         
         if(answer == Integer.MAX_VALUE) 
             answer = -1; 
-               
+        
         return answer;
     }
     
     static void bfs() {
         Queue<Node> que = new ArrayDeque<>(); 
-        boolean[][] visit = new boolean[N][M];
+        boolean[][] visit = new boolean[N][M]; 
         
         que.offer(new Node(0, 0, 1)); 
-        visit[0][0] = true;
+        visit[0][0] = true; 
         
         while(!que.isEmpty()) {
             Node node = que.poll(); 
             
             if(node.y == N - 1 && node.x == M - 1) {
-                answer = Math.min(answer, node.cnt); 
+                answer = Math.min(answer, node.cnt);
             }
             
             for(int d = 0; d < 4; d++) {
-                int ny = node.y + dy[d];
-                int nx = node.x + dx[d];
+                int ny = node.y + dy[d]; 
+                int nx = node.x + dx[d]; 
                 
-                if(ny < 0 || nx < 0 || ny >= N || nx >= M || visit[ny][nx] || maps[ny][nx] == 0) 
-                    continue;
+                if(ny < 0 || nx < 0 || ny >= N || nx >= M || maps[ny][nx] == 0 || visit[ny][nx])
+                    continue; 
                 
-                que.offer(new Node(ny, nx, node.cnt + 1));
-                visit[ny][nx] = true; 
+                que.offer(new Node(ny, nx, node.cnt + 1)); 
+                visit[ny][nx] = true;
             }
         }
     }
     
     static class Node {
-        int y, x, cnt;
+        int y, x, cnt; 
         
         public Node(int y, int x, int cnt) {
-            this.y = y;
-            this.x = x;
+            this.y = y; 
+            this.x = x; 
             this.cnt = cnt;
         }
     }
